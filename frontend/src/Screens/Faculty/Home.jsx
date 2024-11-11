c266637414eee1a1b8600f7474d9e3770b87df8e
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-11-11 10:47:20 +0530

    updated

--- frontend/src/Screens/Faculty/Home.jsx
+++ frontend/src/Screens/Faculty/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "My Profile"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("My Profile")}
@@ -40,7 +40,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Student Info"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("Student Info")}
@@ -50,7 +50,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Upload Marks"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("Upload Marks")}
@@ -60,7 +60,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Timetable"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("Timetable")}
@@ -70,7 +70,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Notice"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("Notice")}
@@ -80,7 +80,7 @@ const Home = () => {
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Material"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                 }`}
                 onClick={() => setSelectedMenu("Material")}
--- frontend/src/Screens/Student/Home.jsx
+++ frontend/src/Screens/Student/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
             <ul className="flex justify-evenly items-center gap-10 w-full mx-auto my-8">
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${selectedMenu === "Notice"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                   }`}
                 onClick={() => setSelectedMenu("Notice")}
@@ -36,7 +36,7 @@ const Home = () => {
               </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${selectedMenu === "Timetable"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                   }`}
                 onClick={() => setSelectedMenu("Timetable")}
@@ -45,7 +45,7 @@ const Home = () => {
               </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${selectedMenu === "Marks"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                   }`}
                 onClick={() => setSelectedMenu("Marks")}
@@ -54,7 +54,7 @@ const Home = () => {
               </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${selectedMenu === "Material"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                   }`}
                 onClick={() => setSelectedMenu("Material")}
@@ -64,7 +64,7 @@ const Home = () => {
 
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${selectedMenu === "My Profile"
-                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm text-black"
                     : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
                   }`}
                 onClick={() => setSelectedMenu("My Profile")}
